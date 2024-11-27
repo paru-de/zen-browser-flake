9fb180660daf4ef6c01ce61417ed5110b298a78a
paru-de <13712334+paru-de@users.noreply.github.com>
2024-11-27 15:36:35 +0100

    fix hash mismatch

--- flake.nix
+++ flake.nix
@@ -14,11 +14,11 @@
     downloadUrl = {
       "specific" = {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-        sha256 = "01i3771m4m55703vigpa1wkj41gny7ii76vx4v95hj63i9rdk5xq";
+        sha256 = "sha256:1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
       };
       "generic" = {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-        sha256 = "1zyz61lhr95kzcbfrqfxygcxmi8v97ffc9bdbrz3xl2ai7fzivwn";
+        sha256 = "sha256:1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
       };
     };
 
